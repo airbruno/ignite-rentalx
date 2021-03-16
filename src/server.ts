ccaa18520c6a9c4b61910870ca83db43a49c292c
brunoribeiro-py <the_bruno15@hotmail.com>
2021-03-16 11:50:01 -0300

    feat: added specifications route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 import { categoryRoutes } from "./routes/category.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/categories", categoryRoutes);
+app.use("/specifications", specificationsRoutes);
 
 app.listen(3333, () => console.log("Server is up and running!"));
