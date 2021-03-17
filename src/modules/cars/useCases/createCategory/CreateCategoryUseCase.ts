382f8a1d1eedaaa41db4a5966a8354c481d179b1
brunoribeiro-py <the_bruno15@hotmail.com>
2021-03-17 08:16:50 -0300

    refactor: useCases folder

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,4 +1,4 @@
-import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
