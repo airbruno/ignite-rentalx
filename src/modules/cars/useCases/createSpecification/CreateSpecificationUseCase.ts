ae8a9008dcadeda4c2197022dee27d08d94c0c6f
brunoribeiro-py <the_bruno15@hotmail.com>
2021-03-17 11:34:53 -0300

    refactor: singletonPatern for SpecificationsRepository

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,11 +1,11 @@
-import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-class CreateSpecificationService {
+class CreateSpecificationUseCase {
   constructor(private specificationsRepository: ISpecificationsRepository) {}
   execute({ name, description }: IRequest): void {
     const specificationAlreadyExists = this.specificationsRepository.findByName(
@@ -16,11 +16,7 @@ class CreateSpecificationService {
       throw new Error("Specification already exists!");
     }
 
-    this.specificationsRepository.create({
-      name,
-      description,
-    });
+    this.specificationsRepository.create({ name, description });
   }
 }
-
-export { CreateSpecificationService };
+export { CreateSpecificationUseCase };
