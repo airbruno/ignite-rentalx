07a7989a7defe48551f58a2e6e02d2b68c5b94bd
Bruno Ribeiro <bribeiro21@icloud.com>
2021-04-06 17:28:56 -0300

    refactor: changed avatarFile constant

--- src/modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarController.ts
@@ -6,7 +6,7 @@ import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 class UpdateUserAvatarController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.user;
-    const avatarFile = null;
+    const avatarFile = request.file.filename;
 
     const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
 
