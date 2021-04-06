17263eb56f2107d81b32245f613731228682766f
Bruno Ribeiro <bribeiro21@icloud.com>
2021-04-06 17:30:18 -0300

    feat: if user already has an avatar, deletes it

--- src/modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/UpdateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -14,6 +15,10 @@ class UpdateUserAvatarUseCase {
   ) {}
   async execute({ user_id, avatarFile }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar${user.avatar}`);
+    }
 
     user.avatar = avatarFile;
 
